721442776990851bd4609612db5ae5f064be4c86
Alex Covone <alexcovone@gmail.com>
2022-02-07 12:58:13 -0500

    Utilized paseInt function

--- js/squashTheBugs.js
+++ js/squashTheBugs.js
@@ -1,4 +1,4 @@
-// Eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number. There will only be one 'longest' word.
+// Eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number. There will only be one 'longest' word
 
 function findLongest(string) {
     return  string.split(" ").reduce((longest, word) => Math.max(word.length, longest),0);
