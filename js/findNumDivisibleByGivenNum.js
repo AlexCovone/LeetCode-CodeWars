3553da5b5eecd2b06dff260efc75d4cc5af16f5d
Alex Covone <alexcovone@gmail.com>
2022-02-09 17:29:37 -0500

    Returned positive odd numbers below n

--- js/findNumDivisibleByGivenNum.js
+++ js/findNumDivisibleByGivenNum.js
@@ -1,7 +1,7 @@
 // Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
 
 function divisibleBy(numbers, divisor){
-    return numbers.map(element => element % divisor === 0)
+    return numbers.filter(element => element % divisor === 0)
 }
 
 // Want to use filter method over map.
