67164204d4550b17ad35ed1845a30297e3bdda11
Alex Covone <alexcovone@gmail.com>
2022-07-04 11:55:33 -0400

    solution001 completed

--- js/solution001.js
+++ js/solution001.js
@@ -1,1 +1,19 @@
-alert('working')
+/* Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9. */
+
+function squareSum(numbers){
+    let squareNum = numbers.map(element => Math.pow(element, 2))
+
+    return squareNum.reduce((acc, c) => acc + c, 0) 
+}
+
+// Alternative
+
+// function squareSum(numbers){
+//     return numbers.reduce(function(sum, n){
+//         return (n*n) + sum;
+//     }, 0)
+// }
+
+
