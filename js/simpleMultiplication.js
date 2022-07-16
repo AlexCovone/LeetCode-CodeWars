d19baf0d05a80f066292f0de8f56adffb1b808b6
Alex Covone <alexcovone@gmail.com>
2022-07-16 13:55:55 -0400

    Added additional optimized solution

--- js/simpleMultiplication.js
+++ js/simpleMultiplication.js
@@ -13,3 +13,5 @@ function simpleMultiplication(number){
 function simpleMultiplication(number){
     return number % 2 === 0 ? number * 8 : number * 9
 }
+
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9
