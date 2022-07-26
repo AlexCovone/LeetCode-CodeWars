18aa0dd73afb507c644ccb3d214d0b29ab3b0667
Alex Covone <alexcovone@gmail.com>
2022-07-26 11:32:49 -0400

    Found the integer that appears as odd number of times in an array

---
+++ js/6kyu/findTheOddInt.js
@@ -0,0 +1,10 @@
+/*
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+*/
+
+function findOdd(A){
+    return A.find((item) => A.filter(element => element == item).length % 2)
+}
+
