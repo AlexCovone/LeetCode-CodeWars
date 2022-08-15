ffff62c77107430fe61b9f42fa48dff8bff05b5f
Alex Covone <alexcovone@gmail.com>
2022-08-15 10:25:06 -0400

    Added increment expression to avoid infinite loop

--- js/unfinishedLoop.js
+++ js/unfinishedLoop.js
@@ -1,4 +1,4 @@
-// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop
 
 function createArray(number){
     var newArray = [];
