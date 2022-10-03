1892edb9c0501208e77e0fd6db24603a8bc7ec0d
Alex Covone <alexcovone@gmail.com>
2022-10-03 10:28:25 -0400

    Added comments

--- js/6kyu/replaceWithAlphabetPosition.js
+++ js/6kyu/replaceWithAlphabetPosition.js
@@ -10,10 +10,12 @@ function alphabetPosition(text){
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
     return text.toLowerCase().split('').filter(element => {
+        //Returns first index at which element can be found
         let index = alphabet.indexOf(element);
 
         return index >= 0;
     })
+    //Values are zero-indexed; will give the value +1
     .map(element => alphabet.indexOf(element) + 1)
     .join(' ')
 }
