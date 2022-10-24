b762c8936c2a3c8c08d9e572aba4209857ed594b
Alex Covone <alexcovone@gmail.com>
2022-10-24 10:40:02 -0400

    fixed spacing

--- js/7kyu/anagramDetection.js
+++ js/7kyu/anagramDetection.js
@@ -1,8 +1,6 @@
 /*
 An anagram is the result of rearranging the letters of a word to produce a new word.
-
 Note: anagrams are case insensitive
-
 Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
 */
 
