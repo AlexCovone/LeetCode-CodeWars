2b1deea3d54c1e8e895043e0f66e48848ab8981e
Alex Covone <alexcovone@gmail.com>
2022-11-10 09:03:05 -0500

    Returned list of integers from 1 to n

---
+++ js/preFizzBuzzWorkout.js
@@ -0,0 +1,17 @@
+/*
+function preFizz(n) {
+  let arr = [];
+  for(let i = 1; i <= n; i++){
+    arr.push(i)
+  }
+  return arr 
+}
+*/
+
+function preFizz(n) {
+    let arr = [];
+    for(let i = 1; i <= n; i++){
+      arr.push(i)
+    }
+    return arr 
+  }
