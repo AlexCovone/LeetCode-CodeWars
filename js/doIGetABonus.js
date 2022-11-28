e02959fa4ecf319422a1a441bbfb9f3187003253
Alex Covone <alexcovone@gmail.com>
2022-11-28 14:01:32 -0500

    Added alternative solution use ES6

--- js/doIGetABonus.js
+++ js/doIGetABonus.js
@@ -5,6 +5,8 @@ Build a function that takes in two arguments (salary, bonus). Salary will be an 
 
 If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
 */
+// ES6
+const bonusTime = (salary, bonus) => bonus ? `£${salary * 10}` : `£${salary}`
 
 function bonusTime(salary, bonus){
     return bonus === true ? '£' + salary * 10 : '£' + salary
