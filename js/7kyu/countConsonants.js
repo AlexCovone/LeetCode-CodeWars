3ef73ae8c6c55bb501420b2b7e7e9916a7c02263
Alex Covone <alexcovone@gmail.com>
2023-01-07 12:05:29 -0500

    Return count of consonant from 1 word string

---
+++ js/7kyu/countConsonants.js
@@ -0,0 +1,20 @@
+/*
+Write a function consonantCount, consonant_count or ConsonantCount that takes a string of English-language text and returns the number of consonants in the string.
+
+Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+
+This function is written to intake a string with one word
+*/
+
+function consonantCount(str) {
+    str = str.toLowerCase()
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let sumConsonant = 0
+
+    for(let char of str){
+        if(!vowels.includes(char)){
+            sumConsonant++
+        }
+    }
+    return sumConsonant
+}
