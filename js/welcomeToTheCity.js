82fef514eed45639dabba9c46509c1ccb5906fea
Alex Covone <alexcovone@gmail.com>
2023-01-09 10:42:03 -0500

    Created function that intakes name array, city and state, and returns welcome message

---
+++ js/welcomeToTheCity.js
@@ -0,0 +1,16 @@
+// Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+function sayHello(name, city, state){
+    let nameArr = []
+    for(let i = 0; i < name.length; i++){
+        nameArr.push(name[i])
+    }
+    nameStr = nameArr.join(' ')
+
+    return `Hello, ${nameStr}! Welcome to ${city}, ${state}!`
+}
+
+// Optimized
+function sayHello(name, city, state){
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
