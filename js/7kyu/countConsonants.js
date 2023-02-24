e64cd8d87510c34fcfbe85c69c4e6d7cb4331b70
Alex Covone <alexcovone@gmail.com>
2023-02-24 10:41:49 -0500

    Returned count of consonants in string

--- js/7kyu/countConsonants.js
+++ js/7kyu/countConsonants.js
@@ -6,6 +6,10 @@ Consonants are all letters used to write English excluding the vowels a, e, i, o
 This function is written to intake a string with one word
 */
 
+// Regex
+const consonantCount = str => str.replace(/[^a-z]|[aeiou]/gi, '').length;
+
+// Does not account for spaces / special characters
 function consonantCount(str) {
     str = str.toLowerCase()
     const vowels = ['a', 'e', 'i', 'o', 'u']
