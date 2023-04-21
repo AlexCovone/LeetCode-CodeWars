81ef366193914ac3f7a1ee864edbd88cc35b17d0
Alex Covone <alexcovone@gmail.com>
2023-04-21 12:32:29 -0400

    Adjusted spacing

--- js/LeetCode/Easy/mergeTwoSortedLists.js
+++ js/LeetCode/Easy/mergeTwoSortedLists.js
@@ -21,37 +21,37 @@ function mergeTwoLists(list1, list2){
     const mergedList = list1.concat(list2)
     // sort merged list
     return mergedList.sort((a, b) => a - b)  
-  }
+}
   
-  function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-  }
+function ListNode(val, next) {
+this.val = (val===undefined ? 0 : val)
+this.next = (next===undefined ? null : next)
+}
   
-  // Using original nodes (no copies)
-  function mergeTwoLists(l1, l2){
+// Using original nodes (no copies)
+function mergeTwoLists(l1, l2){
     const dummy = new ListNode(-Infinity)
     let prev = dummy
-  
+
     while(l1 && l2){
-      if(l1.val <= l2.val){
+        if(l1.val <= l2.val){
         prev.next = l1
         prev = l1
         l1 = l1.next
-      }else{
+        }else{
         prev.next = l2
         prev = l2
         l2 = l2.next
-      }
+        }
     }
-  
+
     if(!l1) prev.next = l2
     if(!l2) prev.next = l1
-  
+
     return dummy.next
-  }
-  
-  console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]), [1, 1, 2, 3, 4, 4])
-  console.log(mergeTwoLists([], []), [])
-  console.log(mergeTwoLists([], [1, 2, 3]), [1, 2, 3])
-  console.log(mergeTwoLists([4, 6, 8, 9], [3, 5, 7]), [3, 4, 5, 6, 7, 8, 9])
+}
+
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]), [1, 1, 2, 3, 4, 4])
+console.log(mergeTwoLists([], []), [])
+console.log(mergeTwoLists([], [1, 2, 3]), [1, 2, 3])
+console.log(mergeTwoLists([4, 6, 8, 9], [3, 5, 7]), [3, 4, 5, 6, 7, 8, 9])
