60991c033bc5c0e24c4f5db930f2fc14cae781cd
Alex Covone <alexcovone@gmail.com>
2023-04-26 18:39:03 -0400

    Return indices that sum to target argument

--- js/LeetCode/Easy/TwoSum.js
+++ js/LeetCode/Easy/TwoSum.js
@@ -1,39 +1,48 @@
-/*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// arr of nums, integers, can be negative, never empty - at least length = 2, target will always exist within the nums arr, no special chars, only valid answer
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// return indices of the 2 numbers that equate to target argument, return in arr  (e.g. [0, 1])
 
-You can return the answer in any order.
+// nums = [2, 7, 11, 15], target = 9 -> [0, 1]
+// nums = [3, 2, 4], target = 6 -> [1, 2]
+// nums = [3, 3], target = 6 -> [0, 1]
+// nums = [5, 2, 1, 8], target = 10 -> [1, 3]
 
- 
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-*/
-
-var twoSum = function(nums, target) {
-    const previousValues = {}
-
-    for(let i = 0; i < nums.length; i++){
-        const currentNumber = nums[i] 
-        const neededValue = target - currentNumber 
-        
-        const index2 = previousValues[neededValue] 
-        if(index2 != null){
-            return [index2, i] 
-        }else{
-            previousValues[currentNumber] = i 
-        }
+// Brute Force
+// Time Complexity - O(N^2)
+// Space Complexity - O(1)
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    console.log(i);
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-};
+  }
+}
+
+function twoSum(nums, target) {
+  // create hashmap
+  // store property as num and index
+  const hashmap = {};
+
+  // loop through nums arr
+  for (let i = 0; i < nums.length; i++) {
+    const currentValue = nums[i];
+    const neededValue = target - currentValue;
+
+    if (hashmap.hasOwnProperty(neededValue)) {
+      return [i, hashmap[neededValue]];
+    }
+
+    // checks if value exist in hashmap
+    if (!hashmap[currentValue]) {
+      hashmap[currentValue] = i;
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
+console.log(twoSum([3, 2, 4], 6), [1, 2]);
+console.log(twoSum([3, 3], 6), [0, 1]);
+console.log(twoSum([5, 2, 1, 8], 10), [1, 3]);
