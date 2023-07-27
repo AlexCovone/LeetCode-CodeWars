c675f0612718fecad3bb6944fd4bfeab5093593b
Alex Covone <alexcovone@gmail.com>
2023-07-27 09:47:25 -0400

    create alternative solution using cache and looping through obj and arr

--- js/7kyu/sumAListButIgnoreAnyDuplicates.js
+++ js/7kyu/sumAListButIgnoreAnyDuplicates.js
@@ -5,6 +5,27 @@ For instance, for the list [3, 4, 3, 6] , the function should return 10.
 */
 
 function sumNoDuplicates(numList) {
-    const nonDuplicateList = numList.filter(number => numList.indexOf(number) === numList.lastIndexOf(number))
-    return nonDuplicateList.reduce((acc, c) => acc + c, 0)
+  const nonDuplicateList = numList.filter((number) => numList.indexOf(number) === numList.lastIndexOf(number));
+  return nonDuplicateList.reduce((acc, c) => acc + c, 0);
+}
+
+// Using cache
+function sumFirstOccurences(arr) {
+  const cache = {};
+  let sum = 0;
+
+  for (const num of arr) {
+    if (num in cache) {
+      cache[num]++;
+    } else {
+      cache[num] = 1;
+    }
   }
+
+  for (const key in cache) {
+    if (cache[key] === 1) {
+      sum += parseInt(key);
+    }
+  }
+  return sum;
+}
