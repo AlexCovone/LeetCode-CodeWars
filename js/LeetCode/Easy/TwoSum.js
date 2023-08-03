a3d0b778bf5aaed3f7e7f80f76ca37994f58b1a0
Alex Covone <alexcovone@gmail.com>
2023-08-03 10:47:59 -0400

    return two indices of values that add up to target argument

--- js/LeetCode/Easy/TwoSum.js
+++ js/LeetCode/Easy/TwoSum.js
@@ -21,29 +21,43 @@ function twoSum(nums, target) {
   }
 }
 
-function twoSum(nums, target) {
-  // create hashmap
-  // store property as num:index
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// ------------------------------------------------
+
+// Two arguments - 1) arr of nums: type number, 2) target: type number
+
+// Return indices in an array of the nums that add up to target (any order) - always be length of 2
+// No manipulating array argument - need to account for indices
+
+var twoSum = function (nums, target) {
+  // create hashmap data structure to store values with associated index
   const hashmap = {};
 
-  // loop through nums arr
+  // loop through each val in nums
   for (let i = 0; i < nums.length; i++) {
     const currentValue = nums[i];
     const neededValue = target - currentValue;
 
-    // checks if hashmap has neededValue already - if so, return two indices in array format
-    if (hashmap.hasOwnProperty(neededValue)) {
-      return [i, hashmap[neededValue]];
-    }
-
-    // checks if value does not exist in hashmap
-    if (!hashmap[currentValue]) {
+    // check if needed value exist in hashmap
+    // if needed value exist, return needed value index + current value index in arr
+    if (neededValue in hashmap) {
+      return [hashmap[neededValue], i];
+    } else {
+      // if not, store current value in hashmap with index
       hashmap[currentValue] = i;
     }
   }
-}
+};
+
+// nums = [2, 7, 11, 15], target = 9 -> return [0, 1]
+// nums = [3, 2, 4], target = 6 -> return [1, 2]
+// nums = [2, 5, 6, 7, 1, 8], target = 15 -> return [3, 5]
 
 console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
 console.log(twoSum([3, 2, 4], 6), [1, 2]);
-console.log(twoSum([3, 3], 6), [0, 1]);
-console.log(twoSum([5, 2, 1, 8], 10), [1, 3]);
+console.log(twoSum([2, 5, 6, 7, 1, 8], 15), [3, 5]);
