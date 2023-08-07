cdea295ec50a1680ec5432291376e226b178070e
Alex Covone <alexcovone@gmail.com>
2023-08-07 12:34:56 -0400

    add alt solution with O(n) space complexity

--- js/LeetCode/Medium/productOfArrayExceptSelf.js
+++ js/LeetCode/Medium/productOfArrayExceptSelf.js
@@ -39,6 +39,31 @@ var productExceptSelf = function (nums) {
   return result;
 };
 
+// Space Complexity O(n)
+var productExceptSelf = function (nums) {
+  const n = nums.length;
+
+  const result = new Array(n);
+  let prefix = new Array(n);
+  let postfix = new Array(n);
+
+  prefix[0] = 1;
+  for (let i = 1; i < n; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
+  }
+
+  postfix[n - 1] = 1;
+  for (let i = n - 2; i >= 0; i--) {
+    postfix[i] = postfix[i + 1] * nums[i + 1];
+  }
+
+  for (let i = 0; i < n; i++) {
+    result[i] = prefix[i] * postfix[i];
+  }
+
+  return result;
+};
+
 // nums = [1, 2, 3, 4] -> return [24, 12, 8, 6]
 // nums = [-1, 1, 0, -3, 3] -> return [0, 0, 9, 0, 0]
 // nums = [5, 2, 7, 1] -> return [14, 35, 10, 70]
