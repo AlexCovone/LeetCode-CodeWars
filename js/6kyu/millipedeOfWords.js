25aabed413a20dc6253e7c35cf74b0327cf0531c
Alex Covone <alexcovone@gmail.com>
2023-09-16 11:34:10 -0400

    rm react component

--- js/6kyu/millipedeOfWords.js
+++ js/6kyu/millipedeOfWords.js
@@ -14,74 +14,36 @@ Millipede: presidenT Trade.
 */
 
 function canCombineWords(words) {
-    // depth first search algo
-    function dfs(usedWords, lastLetter) {
-      if (usedWords.size === words.length) {
-        // All words have been used
-        return true;
-      }
-      
-      for (let i = 0; i < words.length; i++) {
-        if (!usedWords.has(i) && words[i][0] === lastLetter) {
-          usedWords.add(i);
-          if (dfs(usedWords, words[i].slice(-1))) {
-            return true;
-          }
-          usedWords.delete(i);
-        }
-      }
-      
-      return false;
+  // depth first search algo
+  function dfs(usedWords, lastLetter) {
+    if (usedWords.size === words.length) {
+      // All words have been used
+      return true;
     }
-    
 
     for (let i = 0; i < words.length; i++) {
-      if (dfs(new Set([i]), words[i].slice(-1))) {
-        return true;
+      if (!usedWords.has(i) && words[i][0] === lastLetter) {
+        usedWords.add(i);
+        if (dfs(usedWords, words[i].slice(-1))) {
+          return true;
+        }
+        usedWords.delete(i);
       }
     }
-    
+
     return false;
   }
 
-
-  import { getImageUrl } from './utils.js';
-
-  function Avatar({ person, size }) {
-  
-    let thumbnailSize = 's'
-    if(size > 90){
-      thumbnailSize = 'b'
+  for (let i = 0; i < words.length; i++) {
+    if (dfs(new Set([i]), words[i].slice(-1))) {
+      return true;
     }
-    
-    return (
-      <img
-        className="avatar"
-        src={getImageUrl(person, thumbnailSize)}
-        alt={person.name}
-        width={size}
-        height={size}
-      />
-    );
   }
-  
-  export default function Profile() {
-    return (
-      <Avatar
-        size={40}
-        person={{ 
-          name: 'Gregorio Y. Zara', 
-          imageId: '7vQD0fP'
-        }}
-      />
-    );
-  }
-  
-  
 
-
+  return false;
+}
 
 // P - array of words (str)
-// R - boolean value if all words in the array can be combined into one word - last letter of one word and first letter of next must be same 
+// R - boolean value if all words in the array can be combined into one word - last letter of one word and first letter of next must be same
 // E - [excavate, endure, desire, screen, theater, excess, night] => desirE EndurE ExcavatE ExcesS ScreeN NighT Theater = True
 // P - look at each words first and last letter
