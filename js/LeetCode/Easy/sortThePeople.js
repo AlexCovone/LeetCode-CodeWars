de2a86d37ead10d6d97f8a63f3982bca83820b38
Alex Covone <alexcovone@gmail.com>
2023-10-07 12:41:49 -0400

    prettier config

--- js/LeetCode/Easy/sortThePeople.js
+++ js/LeetCode/Easy/sortThePeople.js
@@ -14,42 +14,43 @@ Return names sorted in descending order by the people's heights.
 // return names sorted (mutate) in desc order by people's heights
 
 var sortPeople = function (names, heights) {
-    let map = new Map()
+  let map = new Map();
 
-    for(let i = 0; i < names.length; i++){
-        // all heights are distinct - cannot overwrite
-        map.set(heights[i], names[i])
-    }
+  for (let i = 0; i < names.length; i++) {
+    // all heights are distinct - cannot overwrite
+    map.set(heights[i], names[i]); // {180 : Mary, 165 : John, 170 : Emma}
+  }
 
-    // sort heights arr
-    heights.sort((a, b) => b - a)
+  // initialize arr to append names and return
+  const result = [];
 
-    const result = []
+  // sort heights arr
+  heights.sort((a, b) => b - a);
 
-    // append map[height] (name) to array in desc order
-    for(let height of heights){
-        result.push(map.get(height))
-    }
-    
-    return result
+  // append map[height] (name) to array in desc order
+  for (let height of heights) {
+    result.push(map.get(height)); // get corresponding name with respective height
+  }
+
+  return result;
 };
 
 var sortPeople = function (names, heights) {
-    const people = []
+  const people = [];
 
-    for(let i = 0; i < names.length; i++){
-        people.push({name: names[i], height: heights[i]})
-    }
+  for (let i = 0; i < names.length; i++) {
+    people.push({ name: names[i], height: heights[i] });
+  }
 
-    people.sort((a, b) => b.height - a.height)
+  people.sort((a, b) => b.height - a.height);
 
-    const sortedNames =  people.map(person => person.name)
+  const sortedNames = people.map((person) => person.name);
 
-    return sortedNames
-}
+  return sortedNames;
+};
 
 // names = ["Mary", "John", "Emma"], heights = [180, 165, 170] => return ["Mary", "Emma", John]
 // names = ["Alice", "Bob", "Bob"], heights= [155, 185, 150] => return ["Bob", "Alice", "Bob"]
 
-console.log(sortPeople(["Mary", "John", "Emma"], [180, 165, 170]), ["Mary", "Emma", "John"])
-console.log(sortPeople(["Alice", "Bob", "Bob"], [155, 185, 150]), ["Bob", "Alice", "Bob"])
+console.log(sortPeople(["Mary", "John", "Emma"], [180, 165, 170]), ["Mary", "Emma", "John"]);
+console.log(sortPeople(["Alice", "Bob", "Bob"], [155, 185, 150]), ["Bob", "Alice", "Bob"]);
